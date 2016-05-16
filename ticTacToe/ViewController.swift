b897d01c9712008578a71fa6853f6563e92a992e
Arsames Qajar <acqajar@gmail.com>
2016-05-16 13:26:30 -0700

    Player switching and pictures switching for different players

--- ticTacToe/ViewController.swift
+++ ticTacToe/ViewController.swift
@@ -9,9 +9,53 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    
+    var player: Bool = true
+    
+    var score1: Int = 0
+    var score2: Int = 0
+    
+    
+    
+    @IBOutlet var player1Score: UILabel!
+    
+    @IBOutlet var player2Score: UILabel!
+    
+    
+    @IBOutlet var scoreLabel: UILabel!
+    
+    @IBOutlet var playerImage: UIImageView!
+    
+    
+    
+    @IBAction func playerGrid(sender: AnyObject) {
+       
+        scoreLabel.text = ""
+        
+        if player == true {
+            score1 += 1
+            player1Score.text = "\(score1)"
+            playerImage.image = UIImage(named: "Jay_Ruzesky.jpeg")
+            player = false
+        } else {
+            score2 += 1
+            player2Score.text = "\(score2)"
+            playerImage.image = UIImage(named: "Matthew_Wiebe.jpeg")
+            player = true
+        }
+
+    }
+
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        scoreLabel.text = "Start"
+        
+        
+        
         // Do any additional setup after loading the view, typically from a nib.
     }
 
@@ -20,6 +64,10 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    
+    
+    
+    
 
 }
 
